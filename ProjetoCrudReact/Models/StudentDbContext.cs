9002479c2526fcec26886ed1914cc38f9b934be8
Henrique Rodrigues <ricknet10@gmail.com>
2023-06-11 20:24:24 -0300

    first commit

---
+++ ProjetoCrudReact/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+using System;
+
+namespace ProjetoCrudReact.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+
+        
+    }
+}
